c9eee102f508ed0bf8bf3a5f5398e86a7bdb3c7c
Richard Westenra <rjwestenra@gmail.com>
2019-07-27 16:36:01 +0100

    Move times data list to the index.html
    This is so it's easier to find for anyone who wants to make changes

--- main.js
+++ main.js
@@ -1,43 +1,6 @@
 // Yes I know, it's in ES5. I did it on purpose so that it'll work
 // in older browsers with needing to convert it with Babel, because
 // I couldn't be bothered to look up caniuse.com.
-
-// Base times are in UTC/GMT, via https://www.reddit.com/r/RedDeadOnline/comments/c5wjmc/free_roam_events_schedule/es698ne/
-var schedule = [
-  ['00:00', 'Master Archer'],
-  ['00:45', 'Railroad Baron'],
-  ['01:30', 'Wild Animal Kills'],
-  ['02:15', 'Fishing Challenge'],
-  ['03:00', 'Fool’s Gold'],
-  ['03:45', 'Master Archer'],
-  ['04:30', 'Kills Challenge'],
-  ['05:15', 'King of the Castle'],
-  ['06:00', 'Cold Dead Hands'],
-  ['06:45', 'Dispatch Rider'],
-  ['07:30', 'Railroad Baron'],
-  ['08:15', 'Wild Animal Kills'],
-  ['09:00', 'Fishing Challenge'],
-  ['09:45', 'Fool’s Gold'],
-  ['10:30', 'Master Archer'],
-  ['11:15', 'Side Arm Kills'],
-  ['12:00', 'King of the Castle'],
-  ['12:45', 'Cold Dead Hands'],
-  ['13:30', 'Dispatch Rider'],
-  ['14:15', 'Railroad Baron'],
-  ['15:00', 'Wild Animal Kills'],
-  ['15:45', 'Fishing Challenge'],
-  ['16:30', 'Fool’s Gold'],
-  ['17:15', 'Master Archer'],
-  ['18:00', 'Kills Challenge'],
-  ['18:45', 'King of the Castle'],
-  ['19:30', 'Cold Dead Hands'],
-  ['20:15', 'Dispatch Rider'],
-  ['21:00', 'Railroad Baron'],
-  ['21:45', 'Wild Animal Kills'],
-  ['22:30', 'Fishing Challenge'],
-  ['23:15', 'Fool’s Gold']
-];
-
 var times = document.getElementById('times');
 var nextEventName = document.getElementById('next-event-name');
 var nextEventTime = document.getElementById('next-event-time');
