10e849cd29d595b53189b45258c910baac4d601f
Richard Westenra <rjwestenra@gmail.com>
2019-09-02 16:55:56 +0100

    Add light/dark theme switcher

--- main.js
+++ main.js
@@ -1,142 +1,177 @@
-// Written in ES5 so that it'll work in older browsers
-// without transpilation (if still necessary?)
+// Written in ES5 so that it'll work in older browsers without transpilation.
+// Probably unnecessary but whatever, don't @ me
 
-var eventFrequency = 45 * 60 * 1000;
+(function(){
+  var eventFrequency = 45 * 60 * 1000;
 
-// Select DOM elements
-var times = document.getElementById('times');
-var nextEventName = document.getElementById('next-event-name');
-var nextEventTime = document.getElementById('next-event-time');
-var nextEventEta = document.getElementById('next-event-eta');
-var locale = document.getElementById('locale');
+  // Select DOM elements
+  var times = document.getElementById('times');
+  var nextEventName = document.getElementById('next-event-name');
+  var nextEventTime = document.getElementById('next-event-time');
+  var nextEventEta = document.getElementById('next-event-eta');
+  var locale = document.getElementById('locale');
 
-/**
- * Update the list of event times
- */
-function updateList() {
-  var timesList = document.createElement('ul');
-  schedule.forEach(function(t, i) {
-    var event = calculateEventTimes(t);
-    var li = document.createElement('li');
-    if (event.eta > 0 && event.eta < eventFrequency) {
-      li.classList.add('next-event');
-      nextEventName.innerHTML = event.name;
-      nextEventTime.innerHTML = event.timeString;
-      nextEventEta.innerHTML = event.etaText;
+  /**
+   * Update the list of event times
+   */
+  function updateList() {
+    var timesList = document.createElement('ul');
+    schedule.forEach(function(t, i) {
+      var event = calculateEventTimes(t);
+      var li = document.createElement('li');
+      if (event.eta > 0 && event.eta < eventFrequency) {
+        li.classList.add('next-event');
+        nextEventName.innerHTML = event.name;
+        nextEventTime.innerHTML = event.timeString;
+        nextEventEta.innerHTML = event.etaText;
+      }
+      li.append(getAnchor(i + 1));
+      li.append(event.timeString + ' - ' + event.name);
+      timesList.append(li);
+    });
+    times.innerHTML = timesList.outerHTML;
+  }
+
+  /**
+   * Create an anchor link
+   * @param {number} index
+   * @return {Node} anchor link element
+   */
+  function getAnchor(index) {
+    var anchor = document.createElement('a');
+    anchor.setAttribute('id', index);
+    anchor.setAttribute('href', '#' + index);
+    anchor.innerText = '#';
+    return anchor;
+  }
+
+  /**
+   * Format the event datum and perform time-zone calculations
+   * @param {Array} t Event datum containing time and name
+   * @return {Object} Formatted event datum
+   */
+  function calculateEventTimes(t) {
+    var eventTime = t[0];
+    var now = new Date();
+    var eventDateTime = new Date([now.toDateString(), eventTime, 'UTC'].join(' '));
+    var eta = eventDateTime - now;
+    // Ensure that all event dates are in the future, to fix timezone bug
+    if (eta <= 0) {
+      var tomorrow = new Date();
+      tomorrow.setDate(now.getDate() + 1);
+      eventDateTime = new Date([tomorrow.toDateString(), eventTime, 'UTC'].join(' '));
+      eta = eventDateTime - now;
     }
-    li.append(getAnchor(i + 1));
-    li.append(event.timeString + ' - ' + event.name);
-    timesList.append(li);
+    return {
+      dateTime: eventDateTime,
+      name: t[1],
+      eta: eta,
+      etaText: getEtaText(eta),
+      timeString: eventDateTime.toLocaleTimeString('default', {
+        hour: '2-digit',
+        minute: '2-digit'
+      })
+    };
+  }
+
+  /**
+   * Display time remaining in minutes or seconds
+   * @param {number} t Time in milliseconds
+   * @return {string} e.g. 1 minute, 35 minutes, 1 second, 40 seconds, etc.
+   */
+  function getEtaText(t) {
+    t = t / 1000; // convert to seconds
+    function s(t) {
+      return t === 1 ? '' : 's';
+    }
+    if (t < 60) {
+      return Math.round(t) + ' second' + s(t);
+    }
+    t = Math.round(t / 60); // convert to minutes
+    return t + ' minute' + s(t);
+  }
+
+  /**
+   * Get the name or abbreviation for the user's time zone
+   * @return {string} e.g. GMT or Greenwich Mean Time or UTC+5
+   */
+  function getTimezone() {
+    var date = new Date();
+    // Generate long time-zone name
+    var longTimeZone = date.toString().match(/\((.+)\)/);
+    if (longTimeZone) {
+      return longTimeZone[1];
+    }
+    // Fall back to short code if that one is null
+    var shortTimeZone = date
+      .toLocaleTimeString('en-us', { timeZoneName: 'short' })
+      .split(' ');
+    if (shortTimeZone) {
+      return shortTimeZone[2];
+    }
+    // Finally, fall back to time-zone offset
+    var timeZoneOffset = date.getTimezoneOffset() / -60;
+    if (timeZoneOffset > 0) {
+      timeZoneOffset = '+' + timeZoneOffset;
+    }
+    return 'GMT' + timeZoneOffset;
+  }
+
+  /**
+   * Update the user's time zone in intro paragraph
+   */
+  function showTimeZone() {
+    locale.innerText = ' (' + getTimezone() + ')';
+  }
+
+  // Initialise
+  showTimeZone();
+  updateList();
+
+  // Update event list every 10 seconds
+  window.setInterval(updateList, 10000);
+})();
+
+
+//--- Theme switcher ---//
+(function(){
+  var localStorageKey = 'rdr2-freeroam-schedule-theme';
+  var THEMES = ['dark', 'light'];
+  var themeButton = document.querySelector('#theme');
+  // Detect localstorage value and use that if it exists
+  var currentTheme = localStorage.getItem(localStorageKey) || THEMES[1];
+  updateTheme(currentTheme);
+
+  function updateTheme(theme, updateStorage) {
+    THEMES.forEach(function(d) {
+      document.body.classList.remove(d);
+    });
+    document.body.classList.add(theme);
+    if (updateStorage) {
+      localStorage.setItem(localStorageKey, theme);
+    }
+    currentTheme = theme;
+  }
+
+  // Toggle theme on button click
+  themeButton.addEventListener('click', function() {
+    var newTheme = THEMES.find(function(d) {
+      return d !== currentTheme;
+    });
+    updateTheme(newTheme, true);
   });
-  times.innerHTML = timesList.outerHTML;
-}
+})();
 
-/**
- * Create an anchor link
- * @param {number} index
- * @return {Node} anchor link element
- */
-function getAnchor(index) {
-  var anchor = document.createElement('a');
-  anchor.setAttribute('id', index);
-  anchor.setAttribute('href', '#' + index);
-  anchor.innerText = '#';
-  return anchor;
-}
 
-/**
- * Format the event datum and perform time-zone calculations
- * @param {Array} t Event datum containing time and name
- * @return {Object} Formatted event datum
- */
-function calculateEventTimes(t) {
-  var eventTime = t[0];
-  var now = new Date();
-  var eventDateTime = new Date([now.toDateString(), eventTime, 'UTC'].join(' '));
-  var eta = eventDateTime - now;
-  // Ensure that all event dates are in the future, to fix timezone bug
-  if (eta <= 0) {
-    var tomorrow = new Date();
-    tomorrow.setDate(now.getDate() + 1);
-    eventDateTime = new Date([tomorrow.toDateString(), eventTime, 'UTC'].join(' '));
-    eta = eventDateTime - now;
-  }
-  return {
-    dateTime: eventDateTime,
-    name: t[1],
-    eta: eta,
-    etaText: getEtaText(eta),
-    timeString: eventDateTime.toLocaleTimeString('default', {
-      hour: '2-digit',
-      minute: '2-digit'
-    })
-  };
-}
-
-/**
- * Display time remaining in minutes or seconds
- * @param {number} t Time in milliseconds
- * @return {string} e.g. 1 minute, 35 minutes, 1 second, 40 seconds, etc.
- */
-function getEtaText(t) {
-  t = t / 1000; // convert to seconds
-  function s(t) {
-    return t === 1 ? '' : 's';
-  }
-  if (t < 60) {
-    return Math.round(t) + ' second' + s(t);
-  }
-  t = Math.round(t / 60); // convert to minutes
-  return t + ' minute' + s(t);
-}
-
-/**
- * Get the name or abbreviation for the user's time zone
- * @return {string} e.g. GMT or Greenwich Mean Time or UTC+5
- */
-function getTimezone() {
-  var date = new Date();
-  // Generate long time-zone name
-  var longTimeZone = date.toString().match(/\((.+)\)/);
-  if (longTimeZone) {
-    return longTimeZone[1];
-  }
-  // Fall back to short code if that one is null
-  var shortTimeZone = date
-    .toLocaleTimeString('en-us', { timeZoneName: 'short' })
-    .split(' ');
-  if (shortTimeZone) {
-    return shortTimeZone[2];
-  }
-  // Finally, fall back to time-zone offset
-  var timeZoneOffset = date.getTimezoneOffset() / -60;
-  if (timeZoneOffset > 0) {
-    timeZoneOffset = '+' + timeZoneOffset;
-  }
-  return 'GMT' + timeZoneOffset;
-}
-
-/**
- * Update the user's time zone in intro paragraph
- */
-function showTimeZone() {
-  locale.innerText = ' (' + getTimezone() + ')';
-}
-
-// Initialise
-showTimeZone();
-updateList();
-
-// Update event list every 10 seconds
-window.setInterval(updateList, 10000);
-
-// Initialise Disqus
-var disqus_shortname = 'richardwestenra';
-var dsq = document.createElement('script');
-dsq.type = 'text/javascript';
-dsq.async = true;
-dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-(
-  document.getElementsByTagName('head')[0] ||
-  document.getElementsByTagName('body')[0]
-).appendChild(dsq);
+//--- Initialise Disqus ---//
+(function(){
+  var disqus_shortname = 'richardwestenra';
+  var dsq = document.createElement('script');
+  dsq.type = 'text/javascript';
+  dsq.async = true;
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  (
+    document.getElementsByTagName('head')[0] ||
+    document.getElementsByTagName('body')[0]
+  ).appendChild(dsq);
+})();
